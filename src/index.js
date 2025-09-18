601843e82cad34e2748c1192fab525c4422b50c5
Iskander <iskander.qq@mail.ru>
2025-09-18 21:28:38 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
 import './styles/main.scss'
+
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+
+const swiper = new Swiper('.slider-teacher', {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	modules: [Navigation, Pagination],
+	navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+})
